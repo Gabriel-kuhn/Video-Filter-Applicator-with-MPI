601fccef34fbf2713d0bf8e3ba00677f4780ae29
Gabriel Werner Kuhn <gabrielwernerkuhn1@gmail.com>
2026-06-19 12:02:15 -0300

    feat (process): change filters
    - Change filter in real-time
    - Fix press esc to close
    - Show original image

--- process.cpp
+++ process.cpp
@@ -5,7 +5,33 @@
 #include <mpi.h>
 #include <map>
 
-#define END_TAG 9090
+// TODO tentar negativo
+#define END_TAG 999999 // Não pode ser menor, se não irá conflitar com a lógica de troca de filtro
+#define CICLO_ID 10000
+
+
+
+// .:| ====== Funções Auxiliares ====================================================
+
+// Precisamos reciclar o frame, isso é fazer com que ele resete para 1 quando chegar 
+// no valor limite definido em CICLO_ID
+// Após isso, precisamos formatar a tag que será enviada, para que essa contenha o frama_id e o filtro
+
+// Formatação da TAG:
+// FRAME_RECICLADO + FILTRO
+int get_formated_tag(int current_filter, int frame_id) {
+    int recycled_frame_id = frame_id % CICLO_ID;
+    return (current_filter * CICLO_ID) + recycled_frame_id;
+}
+
+
+int get_recycled_frame(int next_frame_to_show, int received_tag) {
+    return next_frame_to_show + ((received_tag - (next_frame_to_show % CICLO_ID) + CICLO_ID) % CICLO_ID);
+}
+
+
+
+// .:| ====== Funções Principais ====================================================
 
 void start_master(int size, int width, int height, double fps, int total_bytes) {
     cv::VideoCapture capture(0);
@@ -14,8 +40,9 @@ void start_master(int size, int width, int height, double fps, int total_bytes) 
 
     int frame_id = 0;
     int next_frame_to_show = 0;
+    int current_filter = 1;
     bool has_user_exit = false; // variável de controle para saber quando o usuário finalizou o programa
-    
+
     // Matriz de resultado -> buffer
     // Ela serve para guardar os frames procesados
     // Caso algum trabalhador termine seu trabalho mas não é hora de exibir ele ainda, guardamos ele para quando for a hora de exibí-lo
@@ -27,11 +54,13 @@ void start_master(int size, int width, int height, double fps, int total_bytes) 
     for (int i = 1; i < size; i++) {
         if (capture.read(frame)) {
             if (frame.empty()) break;
+
+            int formated_tag = get_formated_tag(current_filter, frame_id);
             
             // A TAG é o próprio número do frame (frame_id)
             // A TAG (id do frame atual) parace inútil, pois o worker vai receber de qualquer TAG (MPI_ANY_TAG), 
             // PORÉM, ela será MUITO importante para a ordenação dos frames no while principal
-            MPI_Send(frame.data, total_bytes, MPI_UNSIGNED_CHAR, i, frame_id, MPI_COMM_WORLD);
+            MPI_Send(frame.data, total_bytes, MPI_UNSIGNED_CHAR, i, formated_tag, MPI_COMM_WORLD);
             frame_id++;
         }
     }
@@ -62,10 +91,13 @@ void start_master(int size, int width, int height, double fps, int total_bytes) 
         // Worker que respondeu -> para enviar mais trabalho especificamente para ele
         // Frame que foi processado -> para adicionar de forma ordenada na lista
         int worker_source = status.MPI_SOURCE;
-        int received_frame_id = status.MPI_TAG;
+        int received_tag = status.MPI_TAG;
+
+        // TODO trasnformar em uma função para facilitar
+        int recycled_frame_id = get_recycled_frame(next_frame_to_show, received_tag);
 
         // O id do frame processado serve como endereço para a lista ordenada
-        buffer[received_frame_id] = received_frame;
+        buffer[recycled_frame_id] = received_frame;
 
         // CRITÉRIO DE RECOMPOSIÇÃO: Verifica se o frame que precisamos exibir agora já está na sala de espera
         while (buffer.find(next_frame_to_show) != buffer.end()) {
@@ -77,20 +109,44 @@ void start_master(int size, int width, int height, double fps, int total_bytes) 
             next_frame_to_show++;
 
             int delay = 1000 / fps;
-            if (cv::waitKey(delay) == 27) {  // 27 = esc
+            int key = cv::waitKey(delay);
+
+            if (key == 27) {  // 27 = esc
                 has_user_exit = true;          
-                break;                        
+                break;    
+
+            } else if (key == '1') {
+                current_filter = 1;
+                printf("[Coordenador] Filtro alterado para: 1 (Imagem Original)\n");
+
+            } else if (key == '2') {
+                current_filter = 2;
+                printf("[Coordenador] Filtro alterado para: 2 (GrayScale)\n");
+
+            } else if (key == '3') {
+                current_filter = 3;
+                printf("[Coordenador] Filtro alterado para: 3 (Sharpen)\n");
             }
         }
 
         if (has_user_exit) {
+            printf("[Coordenador] Encerrando o sistema...\n");
+            
+            // Libera os recursos locais do Master antes de fechar tudo
+            capture.release();
+            cv::destroyAllWindows();
+            
+            // Aborta todos os processos do COMM_WORLD de forma limpa e síncrona
+            MPI_Abort(MPI_COMM_WORLD, 0);
             break;
         }
 
         // Se a câmera ainda tiver frames a serem processados, 
         // O master continua o fluxo enviando para o próximo trabalhador que acabou de ficar livre
         if (capture.read(frame) && !frame.empty()) {
-            MPI_Send(frame.data, total_bytes, MPI_UNSIGNED_CHAR, worker_source, frame_id, MPI_COMM_WORLD);
+            int formated_tag = get_formated_tag(current_filter, frame_id);
+ 
+            MPI_Send(frame.data, total_bytes, MPI_UNSIGNED_CHAR, worker_source, formated_tag, MPI_COMM_WORLD);
             frame_id++;
         }
     }
@@ -118,17 +174,28 @@ void start_worker(int width, int height, int total_bytes) {
 
         // Porém, é avaliado se a TAG enviada não é uma END_TAG (tag de encerramento)
         // Caso seja, o trabalhador sabe que deve parar seu processamento
-        if (status.MPI_TAG == END_TAG) {
+        int tag = status.MPI_TAG;
+        if (tag == END_TAG) {
             break;
         }
 
-        int meu_frame_id = status.MPI_TAG;
+        int current_filter = tag / CICLO_ID;
 
         // Aplica o filtro
-        // TODO ainda precisamos ver uma forma de mudar o filtro a ser processado
-        applySharpen(receive_buffer, send_buffer);
+        if (current_filter == 1) {
+            receive_buffer.copyTo(send_buffer); // Imagem original
+
+        } else if (current_filter == 2) {
+            applyGrayScale(receive_buffer, send_buffer);
+
+        } else if (current_filter == 3) {
+            applySharpen(receive_buffer, send_buffer);
+
+        } else {
+            receive_buffer.copyTo(send_buffer); // caso venha algo inválido mostra a imagem original
+        }
 
         // Envia para o Master o frame já processado e mantendo a TAG recebida
-        MPI_Send(send_buffer.data, total_bytes, MPI_UNSIGNED_CHAR, 0, meu_frame_id, MPI_COMM_WORLD);
+        MPI_Send(send_buffer.data, total_bytes, MPI_UNSIGNED_CHAR, 0, tag, MPI_COMM_WORLD);
     }
 }
